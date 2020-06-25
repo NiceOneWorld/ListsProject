374c04cd139cfb3304082af6f41711bcfcc6d540
Vladyslav <51744989+NiceOneWorld@users.noreply.github.com>
2020-06-25 19:38:37 +0300

    Add files via upload

---
+++ ListsProject/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ListsProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnTrans = new System.Windows.Forms.Button();
+            this.btnRmv = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.btnClr = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(62, 40);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(139, 204);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 265);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(62, 315);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(139, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(367, 40);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox2.Size = new System.Drawing.Size(139, 204);
+            this.listBox2.TabIndex = 3;
+            // 
+            // btnTrans
+            // 
+            this.btnTrans.Location = new System.Drawing.Point(250, 117);
+            this.btnTrans.Name = "btnTrans";
+            this.btnTrans.Size = new System.Drawing.Size(75, 52);
+            this.btnTrans.TabIndex = 4;
+            this.btnTrans.Text = ">>";
+            this.btnTrans.UseVisualStyleBackColor = true;
+            this.btnTrans.Click += new System.EventHandler(this.BtnTrans_Click);
+            // 
+            // btnRmv
+            // 
+            this.btnRmv.Location = new System.Drawing.Point(380, 315);
+            this.btnRmv.Name = "btnRmv";
+            this.btnRmv.Size = new System.Drawing.Size(126, 23);
+            this.btnRmv.TabIndex = 5;
+            this.btnRmv.Text = "Remove";
+            this.btnRmv.UseVisualStyleBackColor = true;
+            this.btnRmv.Click += new System.EventHandler(this.BtnRmv_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(546, 40);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(242, 199);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // btnClr
+            // 
+            this.btnClr.Location = new System.Drawing.Point(595, 315);
+            this.btnClr.Name = "btnClr";
+            this.btnClr.Size = new System.Drawing.Size(131, 23);
+            this.btnClr.TabIndex = 7;
+            this.btnClr.Text = "Clear";
+            this.btnClr.UseVisualStyleBackColor = true;
+            this.btnClr.Click += new System.EventHandler(this.BtnClr_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(227, 263);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(227, 372);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(164, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox2_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnClr);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.btnRmv);
+            this.Controls.Add(this.btnTrans);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnTrans;
+        private System.Windows.Forms.Button btnRmv;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button btnClr;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ ListsProject/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListsProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            listBox1.Items.AddRange(new Object[] { "one", "two", "three", "four", "five" }); ;
+            checkedListBox1.Items.AddRange(new Object[] { "one", "two", "three", "four", "five" }); ;
+            comboBox1.Items.AddRange(new Object[] { "one", "two", "three", "four", "five" }); ;
+            
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (!((string.IsNullOrWhiteSpace(textBox1.Text)
+                || listBox1.Items.Contains(textBox1.Text.Trim()))))
+            {
+                listBox1.Items.Add(textBox1.Text.Trim());
+                textBox1.Text = "";
+            }
+        }
+
+        private void BtnTrans_Click(object sender, EventArgs e)
+        {
+            //foreach (var item in listBox1.SelectedItems)
+            //{
+            //    listBox2.Items.Add(item);
+            //}
+            string[] arr = new string[listBox1.SelectedItems.Count];
+            listBox1.SelectedItems.CopyTo(arr, 0);
+            listBox2.Items.AddRange(arr);
+
+        }
+
+        private void BtnRmv_Click(object sender, EventArgs e)
+        {
+            for (int i = listBox2.SelectedItems.Count-1; i >=0 ; i--)
+            {
+                listBox2.Items.RemoveAt(listBox2.SelectedIndices[i]);
+            }
+        }
+
+        private void BtnClr_Click(object sender, EventArgs e)
+        {
+            checkedListBox1.Items.Clear();
+        }
+
+        private void TextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //MessageBox.Show(e.KeyChar.ToString());
+            if (!(char.IsNumber(e.KeyChar)||e.KeyChar=='\b'))
+                e.Handled = true;
+        }
+    }
+}
